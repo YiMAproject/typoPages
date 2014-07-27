ac4f32b0a03cf100ae811b892440d7ef959c74ff
Payam Naderi <naderi.payam@gmail.com>
2014-07-27 18:47:53 +0430

    check for needed modules on  module init method.

--- src/typoPages/Module.php
+++ src/typoPages/Module.php
@@ -3,7 +3,9 @@ namespace typoPages;
 
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\InitProviderInterface;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\ModuleManagerInterface;
 
 /**
  * Class Module
@@ -11,10 +13,34 @@ use Zend\ModuleManager\Feature\ServiceProviderInterface;
  * @package typoPages
  */
 class Module implements
+    InitProviderInterface,
     ServiceProviderInterface,
     ConfigProviderInterface,
     AutoloaderProviderInterface
 {
+    /**
+     * Initialize workflow
+     *
+     * @param  ModuleManagerInterface $manager
+     *
+     * @return void
+     */
+    public function init(ModuleManagerInterface $moduleManager)
+    {
+        /** @var $moduleManager \Zend\ModuleManager\ModuleManager */
+        $moduleManager->loadModule('yimaAdminor');
+
+        //$moduleManager->loadModule('yimaStaticUriHelper');
+        if (! $moduleManager->getModule('yimaWidgetator')) {
+            // yimaWidgetator needed and not loaded.
+            // loadModule in default zf2 can't load more than one module
+            throw new \Exception(
+                'Module "yimaWidgetator" not loaded, by zf2 module manager we can`t load this module automatically.'
+                .'please enable this module and put before "typoPages".'
+            );
+        }
+    }
+
     /**
      * Expected to return \Zend\ServiceManager\Config object or array to
      * seed such an object.

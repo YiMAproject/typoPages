cee3b2b251f6f1d9cf6c90e1a126310601def176
Payam Naderi <naderi.payam@gmail.com>
2014-07-30 20:55:10 +0430

    improve simple page example

--- src/typoPages/Pages/Simple.php
+++ src/typoPages/Pages/Simple.php
@@ -3,8 +3,23 @@ namespace typoPages\Pages;
 
 use yimaWidgetator\Widget\AbstractWidget;
 
+/**
+ * Class Simple
+ *
+ * @package typoPages\Pages
+ */
 class Simple extends AbstractWidget
 {
+    /**
+     * @var string Content
+     */
+    protected $content;
+
+    /**
+     * @var string Style
+     */
+    protected $style = 'default';
+
     /**
      * Render widget as string output
      *
@@ -12,6 +27,47 @@ class Simple extends AbstractWidget
      */
     public function render()
     {
-        return '<p>This is <strong>Simple</strong> Widget.</p>';
+        return $this->getContent().' With '.$this->getStyle().' Style.';
     }
+
+    /**
+     * Set Content
+     *
+     * @param string $content Content
+     *
+     * @return $this
+     */
+    public function setContent($content)
+    {
+        $this->content = (string) $content;
+
+        return $this;
+    }
+
+    /**
+     * Get Content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStyle()
+    {
+        return $this->style;
+    }
+
+    /**
+     * @param string $style
+     */
+    public function setStyle($style)
+    {
+        $this->style = $style;
+    }
+
 }
